53bff24c58e662f7105896657104862308238fbe
Verney Sandriman <verney@Verneys-Mac-mini.local>
2022-08-02 21:52:24 +0100

    @ROUTING

---
+++ client-app/src/home/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+export default function Homepage(){
+    return (
+        <Container >
+            <h1>Homepage</h1>
+        </Container>
+    )
+}
